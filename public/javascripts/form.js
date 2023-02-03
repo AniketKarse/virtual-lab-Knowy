1ab78a2f5f26f777ae2634c2c8164193ba25b334
shreyash talole <shreyastalole2001@gmail.com>
2023-02-03 12:16:37 +0530

    HTML COMPILER CREATED with Download File Option

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,25 +1,25 @@
 
-const form = document.querySelector("#myForm")
-const Url = '/users/sendForm'
+// const form = document.querySelector("#myForm")
+// const Url = '/users/sendForm'
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    var formData = new FormData(form)
-    var formDataJson = JSON.stringify(Object.fromEntries(formData))
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     var formData = new FormData(form)
+//     var formDataJson = JSON.stringify(Object.fromEntries(formData))
 
-    console.log(formDataJson)
+//     console.log(formDataJson)
     
-    fetch(Url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-        body: formDataJson
-    }).then((response) => {
-        alert(response)
-    }).catch((e) => {
-        // alert(e)
-    })
+//     fetch(Url, {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//           },
+//         body: formDataJson
+//     }).then((response) => {
+//         alert(response)
+//     }).catch((e) => {
+//         // alert(e)
+//     })
 
-})
+// })
--- routes/users.js
+++ routes/users.js
@@ -12,16 +12,28 @@ router.get('/me', (req, res, next) => {
   res.render('dashboard', {name:'Shreyash', class:'10th'})
 })
 
+// PHYSICS
 router.get('/me/physics', (req, res) => {
-  res.render('physics/physicsPract')
+  res.render('2physics/physicsPract')
 })
 
 router.get('/me/physics/pract-1', (req, res) => {
-  res.render('physics/1')
+  res.render('2physics/1')
 })
 
+// HTML
 router.get('/me/html', (req, res) => {
-  res.render('html/htmlPract')
+  res.render('1html/htmlPract')
+})
+
+router.get('/me/html/pract-1', (req, res) => {
+  res.render('1html/html-1')
+})
+
+
+// COMPILER
+router.get('/me/compiler', (req, res) => {
+  res.render('compiler/compiler')
 })
 
 router.post('/sendForm', (req, res, next) => {
