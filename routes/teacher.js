1b4b8795c35efb6542394f82d71e65e02269165d
shreyash talole <shreyastalole2001@gmail.com>
2023-04-13 11:16:16 +0530

    Teacher Login Implemented

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,7 +1,10 @@
 var express = require('express');
 const User = require('../models/user-model')
 const Teacher = require('../models/teacher-model');
+const jwt = require('jsonwebtoken')
 var router = express.Router();
+
+require('../middleware/verify')
 
 router.get('', (req, res) => {
 
@@ -33,6 +36,48 @@ router.post('/saveTeacher', async (req, res) => {
     await teacher.save()
 
 })
+
+// Login Teacher PAGE
+router.get('/login', (Req, res) => {
+    res.render('admin/teacherLogin')
+})
+
+// To Login Teacher
+router.post('/login', async (req, res) => {
+
+    console.log(req.body)
+
+    const isExist = await User.exists({teacherID: req.body.teacherID})
+  
+    if(isExist == null){
+      res.json({error: "Invalid User ID"});
+      return
+    }
+  
+    // console.log(req.body)
+    const filter = {teacherID : req.body.teacherID};
+    const teacher = await Teacher.findOne(filter);
+    
+  
+    // console.log(req.body.studentID)
+  
+    if(teacher.password != req.body.password){
+      res.json({error: "Invalid Password"});
+      return
+    }
+  
+    teacher.password = null
+  
+    // console.log(user)
+  
+    jwt.sign({ teacher }, "THIS IS SECREAT", (err, token) => {
+      res.json({token: token, teacher: teacher})
+    })
+  
+     
+  
+  })
+
 
 
 
