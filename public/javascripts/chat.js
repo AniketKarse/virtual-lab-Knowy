3c979689f192400772be5faec4df656b140cc7cf
shreyash talole <shreyastalole2001@gmail.com>
2023-04-23 15:44:02 +0530

    Chat Feature updated

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,19 +5,25 @@ function appendChat(data) {
     let mainDiv = document.createElement('div');
     mainDiv.classList.add('card');
 
+    if(typeof data.teacherName === "undefined"){
+        data.teacherName = '--'
+    }
+
+    if(typeof data.answer === "undefined"){
+        data.answer = 'Your Question is under review'
+    }
+
     let markup = `
             <div class="card-header" id="studentName">
-            <h6 style="margin: 0px;">Shreyash Talole <span
+            <h6 style="margin: 0px;">${data.studentName} <span
                     class="badge
                     bg-success">${data.subject}</span></h6>
             </div>
             <div class="card-body">
-                <h5 class="card-title" id="chat_question">What do you
-                    think
-                    the meaning of life is?</h5>
-                <p class="card-text" id="chat_answer">Your question is
-                    under
-                    review</p>
+                <h5 class="card-title" id="chat_question">${data.question}</h5>
+                <p class="card-text" id="chat_answer">${data.answer}</p>
+                <hr>
+                <small>${data.teacherName}</small>
             </div>
 
     `
@@ -25,21 +31,29 @@ function appendChat(data) {
     chatArea.appendChild(mainDiv)
 }
 
-fetch("https://virtuallab-chat-api.onrender.com/chat/getAllChats")
-    .then((res) => res.json())
-    .then((data) => {
-        // console.log(data);
-        let chatArrayLength = data.length
-        while (chatArrayLength--) {
-            appendChat(data[chatArrayLength])
-        }
-    });
+window.addEventListener('load', () => {
+        
+        const loading = document.getElementById("loading")
+        loading.innerHTML = "LOADING..."
+
+        fetch("https://virtuallab-chat-api.onrender.com/chat/getAllChats")
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data);
+                loading.style.display = "none"
+                let chatArrayLength = data.length
+                while (chatArrayLength--) {
+                    appendChat(data[chatArrayLength])
+                }
+            });
+
+})
 
 
 // Send Chat to database
 const questionForm = document.getElementById("questionForm")
 
-questionForm.addEventListener('submit', (e) => {
+questionForm.addEventListener('submit',  (e) => {
     e.preventDefault()
 
     var questionFormData = Object.fromEntries(new FormData(questionForm))
@@ -54,17 +68,9 @@ questionForm.addEventListener('submit', (e) => {
         body: JSON.stringify(questionFormData)
     }
 
-    // fetch('https://virtuallab-chat-api.onrender.com/chat/createChat', options)
-    //     .then(res => res.json())
-    //     .then((data) => {
-    //         console.log(data)
-    //     })
-
-    fetch('https://virtuallab-chat-api.onrender.com/chat/createChat', options)
+     fetch('https://virtuallab-chat-api.onrender.com/chat/createChat', options)
         .then(res => res.json())
-        .then((data) => {
-            console.log(data)
-        })
+        .then()
 
 
 
